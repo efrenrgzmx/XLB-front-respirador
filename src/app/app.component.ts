85fb1d11f14cdd340899ea9f669f9b80bad96c42
Efren Rodriguez <efren.rodriguez@protexa.com.mx>
2020-04-28 17:14:06 -0500

    [add] agregado de funcionalidad para comuniccion y toggle de parametros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,101 +10,145 @@ import { Observable, Subscription } from 'rxjs';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'XLB-resp';
+  title = 'TIDAL';
 
-  start = false;
 
-  PIP = 0;
-  PEEP = 0;
-  FR = 0;
-  VL = 0;
-
-  gaugeInfo : ChartInfo;
-
-  lineChartDataA: Chart.ChartDataSets[];
-  lineChartLabelsA: Array<any>;
-  lineChartOptionsA: any;
-
-  lineChartDataB: Chart.ChartDataSets[];
-  lineChartLabelsB: Array<any>;
-  lineChartOptionsB: any;
-
-  lineChartDataC: Chart.ChartDataSets[];
-  lineChartLabelsC: Array<any>;
-  lineChartOptionsC: any;
-
-  lineChartLegend = true;
-  lineChartType = 'line';
-
-  gaugeType = 'arch';
-
-  @ViewChild('ngChartjsA') private readonly ngChartjsA: NgChartjsDirective;
-  @ViewChild('ngChartjsB') private readonly ngChartjsB: NgChartjsDirective;
-  @ViewChild('ngChartjsC') private readonly ngChartjsC: NgChartjsDirective;
-
+  /**
+   * Subscription
+   * * **/
+  userInfo: UserInfo;
+  settingsInfo: SettingsInfo;
   chartDataSub: Subscription;
   settingsDataSub: Subscription;
 
 
-  constructor( private socket: WebsocketService) {
-    this.gaugeInfo = {chartsData: { PT: 0, VT: 0, FT: 0}};
+  /**
+   * Chart
+   */
+  lineChartDataA: Chart.ChartDataSets[];
+  lineChartLabelsA: Array<any>;
+  lineChartOptionsA: any;
+  lineChartLegend = true;
+  lineChartType = 'line';
+  pointsThreshold = 100;
+  updateLimiterCounter = 0;
+  @ViewChild('ngChartjsA') private readonly ngChartjsA: NgChartjsDirective;
 
+  /**
+   * Behaviour
+   * **/
+  startFlag = false;
+  paramsValues = [0, 0, 0];
+  paramsNames = ['TI', 'RATE', 'VT'];
+  paramsUnits = ['', '', ''];
+  changedValue = 0;
+  paramName = '';
+  paramUnit = '';
+  toggleCount = 0;
+
+  constructor( private socket: WebsocketService) {
     this.chartInitA();
-    this.chartInitB();
-    this.chartInitC();
   }
 
   ngOnInit() {
     this.chartDataSub = this.socket.currentChartData.subscribe(chartData => this.addData(chartData));
     this.settingsDataSub = this.socket.currentSettingsData.subscribe(settingsData => this.onChangeSettings(settingsData));
 
+    this.paramName = this.paramsNames[this.toggleCount];
+    this.changedValue = this.paramsValues[this.toggleCount];
+    this.paramUnit = this.paramsUnits[this.toggleCount];
+
   }
 
   addData(sampleData) {
 
+    if(!this.ngChartjsA)
+      return;
+
     sampleData = JSON.parse(sampleData);
-    this.gaugeInfo = sampleData;
+    this.userInfo = sampleData;
 
-    this.lineChartDataA[0].data.push(sampleData.chartsData.PT);
-    this.lineChartLabelsA.push(Date.now());
+    this.lineChartDataA[0].data.push(this.userInfo.data.chartsData.paw);
+    /*this.lineChartDataA[1].data.push(this.userInfo.data.chartsData.vol);
+    this.lineChartDataA[2].data.push(this.userInfo.data.chartsData.freq);*/
+    this.lineChartLabelsA.push('');
 
-    this.lineChartDataB[0].data.push(sampleData.chartsData.VT);
-    this.lineChartLabelsB.push(Date.now());
+    if(this.lineChartDataA[0].data.length > this.pointsThreshold){
+      this.lineChartDataA[0].data.shift();
+      /*this.lineChartDataA[1].data.shift();
+      this.lineChartDataA[2].data.shift();*/
+      this.lineChartLabelsA.shift();
+    }
 
-    this.lineChartDataC[0].data.push(sampleData.chartsData.FT);
-    this.lineChartLabelsC.push(Date.now());
+    if( this.updateLimiterCounter > 0 ) {
+      this.ngChartjsA.chart.update();
+      this.updateLimiterCounter = 0;
+    }
+    this.updateLimiterCounter++;
   }
 
   chartInitA() {
     this.lineChartDataA = [
       {
-        label: 'PRESION/ TIEMPO',
+        label: 'PAW',
         fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
+        lineTension: 0.05,
+        backgroundColor: 'rgb(255,169,140)',
+        borderColor: 'rgba(0, 0, 0, 0.1)',
         borderCapStyle: 'round',
         borderDashOffset: 1.0,
         borderJoinStyle: 'round',
+        pointBorderColor: 'rgba(0, 0, 0, 0.1)',
+        borderWidth: 1,
+        pointRadius: 0,
+        pointHitRadius: 0,
+        data: [],
+      },
+      /*{
+        label: 'VOLUMEN',
+        fill: false,
+        lineTension: 0.1,
+        backgroundColor: 'rgba(75,255,192,1)',
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+        borderCapStyle: 'round',
+        borderDashOffset: 1.0,
+        borderJoinStyle: 'round',
+        pointBorderColor: 'rgba(0, 0, 0, 0.1)',
+        borderWidth: 1.5,
         pointRadius: 0,
         pointHitRadius: 10,
         data: [],
       },
+      {
+        label: 'FRECUENCIA',
+        fill: false,
+        lineTension: 0,
+        backgroundColor: 'rgba(75,255,192,1)',
+        borderColor: 'rgba(0, 0, 0, 0.1)',
+        borderCapStyle: 'square',
+        borderDashOffset: 1.0,
+        borderJoinStyle: 'bevel',
+        pointBorderColor: 'rgba(0, 0, 0, 0.1)',
+        borderWidth: 1.5,
+        pointRadius: 0,
+        pointHitRadius: 10,
+        data: [],
+      }*/
     ];
     this.lineChartLabelsA  = [];
     this.lineChartOptionsA = {
-      responsive: true,
+      tooltips: {
+        enabled: false
+      },
+      animation: {
+        duration: 0,
+      },
       scales: {
         xAxes: [{
-          type: 'realtime',
-          realtime: {
-            refresh: 20,
-            delay: 700,
-            frameRate: 120
-          },
+
           ticks: {
             fontColor: 'rgba(255,255,255,1)',
-            maxTicksLimit: 10,
+            maxTicksLimit: 1,
           }
         }],
         yAxes: [{
@@ -117,157 +161,65 @@ export class AppComponent implements OnInit {
     };
   }
 
-  chartInitB() {
-    this.lineChartDataB = [
-      {
-        label: 'VOLUMEN/ TIEMPO',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'round',
-        borderDashOffset: 1.0,
-        borderJoinStyle: 'round',
-        pointRadius: 0,
-        pointHitRadius: 10,
-        data: [],
-      },
-    ];
-    this.lineChartLabelsB  = [];
-    this.lineChartOptionsB = {
-      responsive: true,
-      scales: {
-        xAxes: [{
-          type: 'realtime',
-          realtime: {
-            refresh: 20,
-            delay: 700,
-            frameRate: 120
-          },
-          ticks: {
-            fontColor: 'rgba(255,255,255,1)',
-            maxTicksLimit: 10,
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            suggestedMax: 1500,
-            suggestedMin: 0,
-            }
-        }]
-      }
+  onChangeSettings(setInfo) {
+    setInfo = JSON.parse(setInfo);
+    this.settingsInfo = setInfo;
 
-    };
-  }
+    this.paramsValues = [this.settingsInfo.settings.ti,
+      this.settingsInfo.settings.freq,
+      this.settingsInfo.settings.vt];
 
-  chartInitC() {
-    this.lineChartDataC = [
-      {
-        label: 'FLUJO/ TIEMPO',
-        fill: false,
-        lineTension: 0.1,
-        backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
-        borderCapStyle: 'round',
-        borderDashOffset: 1.0,
-        borderJoinStyle: 'round',
-        pointRadius: 0,
-        pointHitRadius: 10,
-        data: [],
-      },
-    ];
-    this.lineChartLabelsC  = [];
-    this.lineChartOptionsC = {
-      responsive: true,
-      scales: {
-        xAxes: [{
-          type: 'realtime',
-          realtime: {
-            refresh: 20,
-            delay: 700,
-            frameRate: 120
-          },
-          ticks: {
-            fontColor: 'rgba(255,255,255,1)',
-            maxTicksLimit: 10,
-          }
-        }],
-        yAxes: [{
-          ticks: {
-            suggestedMax: 100,
-            suggestedMin: -100,
-          }
-        }]
-      }
-    };
-  }
-
-  onPlusPEEP() {
-    this.socket.sendData(`PEEP+`);
-  }
-
-  onSubPEEP() {
-    this.socket.sendData(`PEEP-`);
-  }
-
-  onPlusPIP() {
-    this.socket.sendData(`PIP+`);
-  }
-
-  onSubPIP() {
-    this.socket.sendData(`PIP-`);
-  }
-
-  onPlusFR() {
-    this.socket.sendData(`FRQ+`);
-  }
-
-  onSubFR() {
-    this.socket.sendData(`FRQ-`);
-  }
-
-  onPlusVolumen() {
-    this.socket.sendData(`VOL+`);
-
-  }
-
-  onSubVolumen() {
-    this.socket.sendData(`VOL-`);
-  }
-
-  onChangeSettings(settingsInfo) {
-
-    console.log(settingsInfo);
-
-    settingsInfo = JSON.parse(settingsInfo);
-
-    this.PIP = settingsInfo.settings.PIP;
-    this.VL = settingsInfo.settings.VL;
-    this.FR = settingsInfo.settings.FR;
+    this.changedValue = this.paramsValues[this.toggleCount];
   }
 
   onInitPressed() {
-    this.start = true;
+    this.startFlag = true;
   }
 
   onStopPressed() {
-    this.start = false;
+    this.startFlag = false;
+  }
+
+  onToggleParam() {
+    this.toggleCount ++;
+    if (this.toggleCount >= this.paramsNames.length) {
+      this.toggleCount = 0;
+    }
+    this.changedValue = this.paramsValues[this.toggleCount];
+    this.paramName = this.paramsNames[this.toggleCount];
+  }
+
+  addValue(){
+    this.changedValue ++;
+  }
+
+  subValue(){
+    this.changedValue --;
+  }
+
+  onConfirm() {
+    this.socket.sendData(`%${this.toggleCount === 0 ? this.changedValue : this.paramsValues[0]},${this.toggleCount === 1 ? this.changedValue : this.paramsValues[1]},${this.toggleCount === 2 ? this.changedValue : this.paramsValues[2]}`);
   }
 }
 
-export class ChartInfo {
-  chartsData: {
-    PT: number;
-    VT: number;
-    FT: number;
+
+export class UserInfo {
+  data: {
+    chartsData: { paw: number, freq: number, vol: number }
+    params: { ppeak: number, volmin: number, vte: number, ftotal: number, peep: number, pip: number}
   };
 }
 
-export class SettingsInfo {
+export class SettingsInfo{
   settings: {
-    PIP: number;
-    VL: number;
-    FR: number;
+    ti: number,
+    freq: number,
+    vt: number
   };
 }
 
+
+
+/**
+ * this.socket.sendData(`PIP+`);
+ **/
