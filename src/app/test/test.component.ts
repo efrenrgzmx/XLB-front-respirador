89ca46f5505a0d5cda447204751cab47099ad5b0
Efren Rodriguez <efren.rodriguez@protexa.com.mx>
2020-05-26 18:02:31 -0500

    [update] cambiando proceso al actual pero con estilo en blanco

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -61,7 +61,7 @@ export class TestComponent implements OnInit {
 
       this.finishTests = true;
       await this.delay(200);
-      this.router.navigate(['/patient']);
+      this.router.navigate(['/dashboard']);
     })();
   }
 
